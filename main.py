e6de24741511ff41c7a36ade432f93a8ab2c773b
JeanRibes <jean.christophe.ribes@sfr.fr>
2020-11-19 18:16:47 +0100

    configuration auto de tous les liens entre routeurs en OSPF avec options custom

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ipaddress
 from ipaddress import IPv4Address
 from typing import *
 
@@ -5,10 +6,108 @@ import gns3fy
 
 from util import *
 
+config_custom = { # permet
+    'R8': {
+        'disable': False,
+        'extra': "ipv6 router rip OUI\nredistribute connected",
+        'interfaces': {
+            'f2/0': {
+                'disable': False,
+                'extra': "ipv6 address 2001:0:8::1/64"
+            },
+            'f3/0': {
+                'disable': False,
+            },
+            'f0/0': {
+                'disable': False,
+            }
+        }
+    }
+}
+
+
+def get_extra_interface_conf(router_name: str, interface_name: str):
+    try:
+        return config_custom[router_name]['interfaces'][interface_name]['extra']
+    except:
+        print('err0')
+        return ''
+
+def get_extra_global_conf(router_name:str):
+    try:
+        return config_custom[router_name]['extra']
+    except: return ''
+
+def get_is_interface_disabled(router_name: str, interface_name: str) -> bool:
+    try:
+        return config_custom[router_name]['interfaces'][interface_name]['disable']
+    except:
+        return False
+
+
+def get_is_router_disabled(router_name: str):
+    try:
+        return config_custom[router_name]['disable']
+    except:
+        return False
+
+
+def make_config(router: Node, area: str, ospf_process: int = 1) -> str:
+
+
+    s = f"""###### config pour {router.name} ######
+ipv6 unicast-routing
+router ospfv3 {ospf_process}
+    address-family ipv6 unicast
+        redistribute connected
+        exit-address-family
+    router-id {router.router_id}
+  exit
+{get_extra_global_conf(router.name)}
+end
+conf t"""
+
+    for interface in router.interfaces:
+        if get_is_interface_disabled(router.name,interface.get_name()):
+            continue
+        s += f"""
+int {interface.get_name()}
+# connectee a l'interface {interface.reverse_int()} du routeur {interface.reverse_router().name}
+    no shut
+    ipv6 enable
+    ipv6 address {interface.get_ip()}
+    """
+        if interface.reverse_router().router:
+            s += f"ipv6 ospf {ospf_process} area {area}\n"
+        extra_conf_int = get_extra_interface_conf(router.name, interface.get_name())
+        if len(extra_conf_int)>1:
+            s += extra_conf_int +"\nend\nconf t"  # pour éviter les problèmes
+        else:
+            s += "exit"
+
+    s += f"""
+end
+###### fin de la config pour {router.name} ######
+    """
+
+    return s
+
+
+def apply_config(router: Node, config: str):
+    if get_is_router_disabled(router.name):
+        return None
+    console: Console = router.vty
+    console.write_cmd(b'\r')  # active la console
+    console.write_cmd(b'end')
+    console.write_cmd(b'configure terminal')
+    console.write_conf(config)
+
+
 if __name__ == '__main__':
     gs = gns3fy.Gns3Connector("http://localhost:3080", user="admin",
                               cred="YwrGQJnK6KPO7scnYIxTYiLpVmKc0VKeKwQEo9wMBP6D2oUdMCRJbt1AAh0Sts39")
-    project_id = gs.get_project(name="nas-mpls")['project_id']
+    project_id = gs.get_project(name="test-python-autoconfig")['project_id']
+    #    project_id = gs.get_project(name="nas-mpls")['project_id']
 
     mynodes = NodeRepo()
     # setup initial
@@ -17,9 +116,9 @@ if __name__ == '__main__':
         obj = gns3fy.Node(project_id=project_id, node_id=node['node_id'], connector=gs)
         obj.get()
         myconsole = Console(obj.console_host, obj.console)
-        myconsole.write_cmd(b'\r')  # active la console
 
         obj.vty = myconsole
+        obj.interfaces = []
 
         obj.router = obj.node_type == 'dynamips'
         obj.router_id = str(IPv4Address(router_i))  # génère un router-id en forme a.b.c.d
@@ -27,35 +126,28 @@ if __name__ == '__main__':
 
         mynodes.add(obj)
 
-    # mapping des connexions
-    my_links = []
-    i = 0
-    for node_id, node in mynodes.items():
-        jlinks = []
-        print(f"{node.name} ({node.router_id})")
-        for link in node.links:
-            jlink = JLink()
-            jlink.link_id = i
-            i += 1
-            for link_node in link.nodes:
-                if link_node['node_id'] == node_id:
-                    jlink.source_int = link_node['label']['text']
-                    jlink.source_node = node
-                else:
-                    jlink.dest_int = link_node['label']['text']
-                    jlink.dest_node = mynodes.get(link_node['node_id'])
-            print(f"   ({jlink.source_node.name}):{jlink.source_int}  -->  {jlink.dest_int}:({jlink.dest_node.name})")
+    for _link in gs.get_links(project_id):
+        link = gns3fy.Link(connector=gs, project_id=project_id, link_id=_link['link_id'])
+        link.get()
+        lien = Lien(link.link_id)
+        lien.interface_a = link.nodes[0]['label']['text']
+        lien.side_a = mynodes.get(link.nodes[0]['node_id'])
+        lien.side_b = mynodes.get(link.nodes[1]['node_id'])
+        lien.interface_b = link.nodes[1]['label']['text']
 
-            jlinks.append(jlink)
-            node.jlinks = jlinks
-            my_links.append(jlink)
+        lien.network = '2001:' + link.link_id.split('-')[3]
 
-    # reduce des links
-    for i, right_jlink in enumerate(my_links):
-        print(f"#{right_jlink.link_id}   ({right_jlink.source_node.name}):{right_jlink.source_int}  -->  {right_jlink.dest_int}:({right_jlink.dest_node.name})")
-        filt = lambda jlink:(jlink.dest_node.name == right_jlink.source_node.name) \
-                            and jlink.dest_int == right_jlink.source_int
-        for left_jlink in filter(filt,my_links):
-            print(f"    #{left_jlink.link_id}   ({left_jlink.source_node.name}):{left_jlink.source_int}  -->  {left_jlink.dest_int}:({left_jlink.dest_node.name})")
+        lien.side_a.interfaces.append(Interface(True, lien))
+        lien.side_b.interfaces.append(Interface(False, lien))
 
+        print(lien)
 
+    r8 = mynodes.get_by_name('R8')
+    print(make_config(r8, '0', 9999))
+    exit(0)
+    for node in mynodes.values():
+        if node.router:
+            cfg = make_config(node, "0")
+            print(f"configuration de {node.name} ... ", end="")
+            apply_config(node, cfg)
+            print("fini !")
--- util.py
+++ util.py
@@ -11,7 +11,11 @@ class Console:
     # consoel_port = Node().console
     def __init__(self, console_host, console_port):
         self.sock = socket()
-        self.sock.connect((console_host, console_port))
+        try:
+            self.sock.connect((console_host, console_port))
+        except ConnectionRefusedError:
+            print("Vous devez lancer le projet GNS3 pour configurer les routeurs")
+            exit(0)
         self.sock.recvfrom(100000)
 
     def write_conf(self, text: str):
@@ -23,7 +27,7 @@ class Console:
 
 class NodeRepo(dict):
     def get_by_name(self, name: str):
-        for node in self:
+        for node in self.values():
             if node.name == name:
                 return node
 
@@ -38,3 +42,62 @@ class JLink:
     dest_int: str
     dest_node: Node
     network: Optional[str]
+
+
+class Lien:
+    def __init__(self, lid):
+        self.link_id = lid
+
+    link_id: str
+
+    side_a: Node
+    ip_a: str  # # 2001:0:dead:beef::10
+    interface_a: str
+
+    network: str  # 2001:0:dead:beef
+
+    interface_b: str
+    ip_b: str  # 2001:0:dead:beef::11
+    side_b: Node
+
+    def __str__(self):
+        return f"{self.side_a.name}: {self.interface_a} [{self.get_ipa()}] <-> [{self.get_ipb()}] {self.interface_b}:{self.side_b.name}"
+
+    def get_ipa(self):
+        return f"{self.network}::10/64"
+
+    def get_ipb(self):
+        return f"{self.network}::11/64"
+
+
+class Interface:
+    def __init__(self, sa: bool, l: Lien):
+        self.side_a = sa
+        self.lien = l
+
+    side_a: bool
+    lien: Lien
+
+    def get_ip(self):
+        if self.side_a:
+            return self.lien.get_ipa()
+        else:
+            return self.lien.get_ipb()
+
+    def get_name(self):
+        if self.side_a:
+            return self.lien.interface_a
+        else:
+            return self.lien.interface_b
+
+    def reverse_int(self):
+        if not self.side_a:
+            return self.lien.interface_a
+        else:
+            return self.lien.interface_b
+
+    def reverse_router(self):
+        if not self.side_a:
+            return self.lien.side_a
+        else:
+            return self.lien.side_b
